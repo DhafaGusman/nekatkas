656a0dfee09ae288cfb231d1669c176257525be6
DhafaGusman <dhafgoes25@gmail.com>
2024-06-24 20:54:10 +0700

    Register page minor update

--- lib/utils/colors/global_colors.dart
+++ lib/utils/colors/global_colors.dart
@@ -1,8 +1,8 @@
 import 'package:hexcolor/hexcolor.dart';
 
 class GlobalColors {
-  static HexColor mainColor = HexColor('5AB2FF');
-  static HexColor firstColor = HexColor('0277BD');
+  static HexColor mainColor = HexColor('29B6F6');
+  static HexColor logoColor = HexColor('0277BD');
   static HexColor secondColor = HexColor('A0DEFF');
   static HexColor thirdColor = HexColor('008DDA');
   static HexColor fourthColor = HexColor('999999');
--- lib/view/auth/register_page.dart
+++ lib/view/auth/register_page.dart
@@ -1,4 +1,11 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:feather_icons/feather_icons.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 import '../../utils/colors/global_colors.dart';
 
@@ -10,6 +17,106 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  // -- Variable -- //
+  final int _pageIndex = 0;
+
+  int countdown = 60;
+  Timer? timer;
+  StreamController<int> countdownStream = StreamController<int>();
+
+  Uint8List? _image;
+  File? selectedImage;
+
+  // -- Form Key -- //
+  final GlobalKey<FormState> _step1FormKey = GlobalKey();
+  final GlobalKey<FormState> _step2FormKey = GlobalKey();
+  final GlobalKey<FormState> _step3FormKey = GlobalKey();
+  final GlobalKey<FormState> _step4FormKey = GlobalKey();
+  final GlobalKey<FormState> _step5FormKey = GlobalKey();
+  final GlobalKey<FormState> _step6FormKey = GlobalKey();
+
+  // -- Controller -- //
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController telephoneController = TextEditingController();
+  final TextEditingController shopNameController = TextEditingController();
+  final TextEditingController shopCategoryController = TextEditingController();
+
+  // -- Initial State -- //
+  @override
+  void initState() {
+    super.initState();
+    startCountdown();
+  }
+
+  // -- Start Countdown -- //
+  void startCountdown() {
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (countdown > 0) {
+        setState(() {
+          countdown--;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  // -- Dispose -- //
+  @override
+  void dispose() {
+    timer?.cancel();
+    countdownStream.close();
+    super.dispose();
+  }
+
+  // -- List Page for Title -- //
+  final List<Map<String, dynamic>> _step = [
+    {
+      'judul': 'Buat akun',
+      'deskripsi': 'Masukan nama dan email kamu',
+      'Judul': 'Lanjut',
+    },
+    {
+      'judul': 'Masukkan kata sandi',
+      'deskripsi': 'Kata sandi minimal 6 karakter dan 1 angka',
+      'Judul': 'Lanjut',
+    },
+    {
+      'judul': 'Masukkan nomor telepon',
+      'deskripsi': 'Kami akan mengirimkan kode verifikasi\nke telepon mu',
+      'Judul': 'Lanjut',
+    },
+    {
+      'judul': 'Masukkan kode verifikasi',
+      'deskripsi': 'Coba cek kode verifikasi di pesan',
+      'Judul': 'Lanjut',
+    },
+    {
+      'judul': 'Buat toko baru',
+      'deskripsi': 'Masukan nama toko kamu dan kategorinya',
+      'Judul': 'Lanjut',
+    },
+    {
+      'judul': 'Tambah foto',
+      'deskripsi': 'Tambahin foto untuk toko kamu',
+      'Judul': 'Buat akun',
+    }
+  ];
+
+  // -- Controller Register Page -- //
+  void controllerRegister() async {
+    if (_pageIndex == 0) {
+      if (_step1FormKey.currentState!.validate()) {
+        // -- Validasi jika username kosong --/
+        if (usernameController.text.isEmpty || emailController.text.isEmpty) {
+          return;
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,6 +144,7 @@ class _RegisterPageState extends State<RegisterPage> {
         ),
         child: Column(
           children: [
+            const SizedBox(height: 30),
             Image.asset(
               'assets/img/vector/senyum.png',
               width: 200,
@@ -60,10 +168,108 @@ class _RegisterPageState extends State<RegisterPage> {
                 color: GlobalColors.fourthColor,
               ),
             ),
-            const SizedBox(height: 40),
-            TextFormField(),
-            const SizedBox(height: 15),
-            TextFormField(),
+            const SizedBox(height: 50),
+            TextFormField(
+              decoration: InputDecoration(
+                icon: Icon(
+                  FeatherIcons.user,
+                  color: GlobalColors.fourthColor,
+                ),
+                labelText: 'Nama pengguna',
+                labelStyle: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 13,
+                  color: GlobalColors.fourthColor,
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: GlobalColors.mainColor, width: 1.5),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: GlobalColors.garisColor, width: 1.5),
+                ),
+              ),
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 15,
+                color: GlobalColors.textColor,
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                icon: Icon(
+                  FeatherIcons.mail,
+                  color: GlobalColors.fourthColor,
+                ),
+                labelText: 'Alamat email',
+                labelStyle: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 13,
+                  color: GlobalColors.fourthColor,
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: GlobalColors.mainColor, width: 1.5),
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: GlobalColors.garisColor, width: 1.5),
+                ),
+              ),
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontSize: 15,
+                color: GlobalColors.textColor,
+              ),
+            ),
+            const SizedBox(height: 60),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 55),
+                    backgroundColor: HexColor('29B6F6'),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  child: const Text(
+                    'Lanjut',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Sudah punya akun? ',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 13,
+                          color: GlobalColors.fourthColor,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'Masuk',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          color: GlobalColors.mainColor,
+                        ),
+                        recognizer: TapGestureRecognizer()..onTap = () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),

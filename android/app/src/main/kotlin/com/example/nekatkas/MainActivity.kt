10b1e144833a446b25ec755221222a4fcfd6a046
Dhafff <dhafgoes@student.telkomuniversity.ac.id>
2024-06-24 18:35:25 +0700

    Initialize the project

---
+++ android/app/src/main/kotlin/com/example/nekatkas/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nekatkas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nekatkas/utils/colors/global_colors.dart';
+import 'package:nekatkas/view/auth/landing_page.dart';
+// import 'package:nekatkas/view/auth/landing_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const LandingPage()));
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: SystemUiOverlay.values,
+    );
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: GlobalColors.mainColor,
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/img/logo/logo-nekatkas.png',
+              width: 150,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset(
+              'assets/img/vector/uang.png',
+              width: 370,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
